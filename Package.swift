735a5fde4c1c3e74db7f3dfc3e89c3ac98ea2aa5
Alex Tran Qui <alex@katalysis.io>
2020-04-22 10:20:01 +0200

    move to apple swift-cryto

--- Package.swift
+++ Package.swift
@@ -1,17 +1,20 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "jwt-kit",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .library(name: "JWTKit", targets: ["JWTKit", "JWT"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/crypto-kit.git", .exact("4.0.0-alpha.1")),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
      ],
     targets: [
-        .target(name: "JWTKit", dependencies: ["CryptoKit"]),
-        .target(name: "JWT", dependencies: ["CryptoKit", "JWTKit"]),
+        .target(name: "JWTKit", dependencies: [.product(name: "Crypto", package: "swift-crypto")]),
+        .target(name: "JWT", dependencies: [.product(name: "Crypto", package: "swift-crypto"), "JWTKit"]),
         .testTarget(name: "JWTKitTests", dependencies: ["JWTKit"]),
     ]
 )
